ca146b4cb8a9598b3868e9dd273ad566e4858ac7
Elaine Anderson <eparie@bu.edu>
2018-07-03 03:53:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,6 +22,12 @@ end
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end
 
+def self.find_by_name(name)
+  sql = "SELECT * FROM songs WHERE name = ?"
+  result = DB[:conn].execute(sql, name)[0]
+  Song.new(result[0], result[1], result[2])
+end
+
 def update
   sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
   DB[:conn].execute(sql, self.name, self.grade, self.id)
