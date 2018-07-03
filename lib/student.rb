8de0e5f3b085a1ace98eaa9a5c729f0c36675723
Elaine Anderson <eparie@bu.edu>
2018-07-03 03:57:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ def update
 end
 
   def self.new_from_db(row)
-  new_student = self.new(name, grade, id)
+  new_student = self.new(name, grade, id = nil)
 new_student.id = row[0]
 new_student.name =  row[1]
 new_student.grade = row[2]
